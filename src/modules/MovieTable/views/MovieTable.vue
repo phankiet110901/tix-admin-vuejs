c7ed07fcd8ab9d0a98bab2ea369e5a5fc79779c5
phannkiet110901 <phannkiet110901@gmail.com>
2021-03-16 14:27:16 +0700

    add more src

--- src/modules/MovieTable/views/MovieTable.vue
+++ src/modules/MovieTable/views/MovieTable.vue
@@ -3,9 +3,45 @@
     <overlay-loading v-if="isMovieLoading"></overlay-loading>
     <!-- table movie -->
     <div>
-      <v-btn depressed color="primary" style="margin-bottom: 6px;">
-        Add Movie
-      </v-btn>
+      <template>
+        <div>
+          <v-dialog v-model="dialog" width="500">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                Add movie
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title class="headline grey lighten-2">
+                Add movie
+              </v-card-title>
+
+              <v-card-text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="dialog = false">
+                  Add movie
+                </v-btn>
+                <v-btn color="primary" text @click="dialog = false">
+                  Close
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+      </template>
       <v-simple-table>
         <thead>
           <tr>
@@ -75,7 +111,11 @@ export default {
     ...mapState(["MovieTableStore"]),
   },
   created() {
-    this.getMovie(this.$route.params.page);
+    if (this.$route.params.page) {
+      this.getMovie(this.$route.params.page);
+    } else {
+      this.getMovie(1);
+    }
   },
   filters: {
     formatDate: (date) => {
@@ -84,6 +124,11 @@ export default {
         1}/${newDate.getFullYear()}`;
     },
   },
+  data() {
+    return {
+      dialog: false,
+    }
+  }
 };
 </script>
 <style lang="scss">
